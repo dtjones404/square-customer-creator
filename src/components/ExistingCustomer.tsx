34f1d60311aade3b779423e3e3cd07a49953df11
dtjones404 <dustintaylorjones@gmail.com>
2022-05-31 22:29:26 -0400

    Tweaked styling of searchbar.

--- src/components/ExistingCustomer.tsx
+++ src/components/ExistingCustomer.tsx
@@ -22,44 +22,40 @@ export default function ExistingCustomer({
   handleSearchTermChange,
 }: IExistingCustomerProps) {
   return (
-    <div className="flex flex-col gap-2 flex-1">
+    <div className="flex-1 border-2 bg-white rounded-lg max-h-[700px] overflow-y-scroll shadow-xl flex flex-col">
       <Searchbar
         searchTerm={searchTerm}
         handleSearchTermChange={handleSearchTermChange}
       />
-      <div className="border-2 bg-white rounded-lg max-h-[700px] overflow-y-scroll shadow-xl flex flex-col">
-        {!customerData.length ? (
-          <div className="text-xl py-24 font-bold flex flex-col gap-2 justify-center items-center">
-            <FontAwesomeIcon
-              className="text-4xl text-rose-500 mr-2"
-              icon={faPersonCircleQuestion}
-            />
-            <h2 className="text-2xl text-center">No customers found.</h2>
-          </div>
-        ) : (
-          <TransitionGroup>
-            {customerData.map((customerData) => (
-              <TransitionWrapper
-                key={customerData.id}
-                timeout={300}
-                classNames="customerCard"
-              >
-                <Customer
-                  givenName={customerData.given_name}
-                  familyName={customerData.family_name}
-                  emailAddress={customerData.email_address}
-                  createdAt={new Date(customerData.created_at)}
-                  emailUnsubscribed={
-                    customerData.preferences.email_unsubscribed
-                  }
-                  handleDelete={() => handleDelete(customerData.id)}
-                  handleEdit={() => handleEdit(customerData)}
-                />
-              </TransitionWrapper>
-            ))}
-          </TransitionGroup>
-        )}
-      </div>
+      {!customerData.length ? (
+        <div className="text-xl py-24 font-bold flex flex-col gap-2 justify-center items-center">
+          <FontAwesomeIcon
+            className="text-4xl text-rose-500 mr-2"
+            icon={faPersonCircleQuestion}
+          />
+          <h2 className="text-2xl text-center">No customers found.</h2>
+        </div>
+      ) : (
+        <TransitionGroup>
+          {customerData.map((customerData) => (
+            <TransitionWrapper
+              key={customerData.id}
+              timeout={300}
+              classNames="customerCard"
+            >
+              <Customer
+                givenName={customerData.given_name}
+                familyName={customerData.family_name}
+                emailAddress={customerData.email_address}
+                createdAt={new Date(customerData.created_at)}
+                emailUnsubscribed={customerData.preferences.email_unsubscribed}
+                handleDelete={() => handleDelete(customerData.id)}
+                handleEdit={() => handleEdit(customerData)}
+              />
+            </TransitionWrapper>
+          ))}
+        </TransitionGroup>
+      )}
     </div>
   );
 }
--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -1,3 +1,6 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+
 interface ISearchbarProps {
   searchTerm: string;
   handleSearchTermChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
@@ -8,9 +11,13 @@ export default function Searchbar({
   handleSearchTermChange,
 }: ISearchbarProps) {
   return (
-    <div className="px-2">
+    <div className="p-4 border-b-2 flex items-center gap-4">
+      <FontAwesomeIcon
+        className="text-xl text-blue-700"
+        icon={faMagnifyingGlass}
+      />
       <input
-        className="w-full lg:w-[450px] shadow-lg rounded bg-white focus:ring-blue-200/70 focus:bg-white focus:ring-4 invalid:ring-red-300/70 invalid:ring-4"
+        className="w-full lg:w-[450px] shadow-sm rounded bg-white focus:ring-blue-200/70 focus:bg-white focus:ring-4 invalid:ring-red-300/70 invalid:ring-4"
         type="text"
         value={searchTerm}
         onChange={handleSearchTermChange}
